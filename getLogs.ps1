0a8e08ee9dbd66c1c390285c1dd9c2170d60a6bf
Juan Antonio <j.villgmz@gmail.com>
2016-10-24 17:23:12 -0700

    Application to retrive logs from >=1 machines

---
+++ getLogs.ps1
@@ -0,0 +1,22 @@
+﻿$targetComp = Read-Host -Prompt 'What Computer?'
+Write-Host $targetComp
+
+$logDir = "\C$\ProgramData\ETS\IBT2\wks"
+$logDirTEST = "\Users"
+
+
+$targetMachines = net view | select -Skip 1 | ?{$_-match '205-'}
+
+
+foreach($computer IN $targetMachines)
+{
+    #write-host $computer.Substring(0,8)
+    $station = $computer.Substring(0,8)
+
+    $fileLoc = $station + $logDir
+
+    Set-Location $fileLoc
+    Get-ChildItem
+}
+
+#\\200-01\c$
